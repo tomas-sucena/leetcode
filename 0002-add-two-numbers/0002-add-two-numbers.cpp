1a153f90654b73d2de8e75e995dfaaf57b9a95fb
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2023-05-28 11:58:21 +0100

    Time: 40 ms (53.50%), Space: 71.5 MB (11.24%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,11 +14,16 @@ public:
         if (!l1 && !l2 && !carry)
             return nullptr;
 
-        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+        int val1 = l1 ? l1->val : 0;
+        int val2 = l2 ? l2->val : 0;
+        int sum = val1 + val2 + carry;
 
         ListNode* res = new ListNode(sum % 10);
-        res->next = addTwoNumbers(l1 ? l1->next : l1, l2 ? l2->next : l2, sum / 10);      
-        
+
+        l1 = l1 ? l1->next : nullptr;
+        l2 = l2 ? l2->next : nullptr;
+
+        res->next = addTwoNumbers(l1, l2, sum / 10);        
         return res;
     }
 };

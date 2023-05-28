a8ab0217fdca013aa446aebbc6e295a25fcd723f
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2023-05-28 13:11:13 +0100

    Time: 0 ms (100.00%), Space: 5.8 MB (93.72%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    bool overflow(int a, int b) {
+        return ((a > 0) ^ (b > 0)) 
+            ? (a < INT_MIN / b) 
+            : (a > INT_MAX / b);
+    }
+
+public:
+    int reverse(int x) {
+        int rev = 0;
+        for (; x; x /= 10) {
+            if (overflow(rev, 10))
+                return 0;
+
+            rev *= 10;
+            rev += x % 10;
+        }
+
+        return rev;
+    }
+};

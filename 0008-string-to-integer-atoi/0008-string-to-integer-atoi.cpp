56dc32246a975dec7df13689d1fb70924a15ab52
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2023-10-28 22:36:00 +0100

    Time: 3 ms (58.78%), Space: 6.7 MB (28.71%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        long n = 0;
+        bool negative = false;
+
+        auto it = s.begin();
+
+        // ignore whitespace
+        while (it != s.end() && *it == ' ')
+            ++it;
+
+        if (it == s.end())
+            return n;
+
+        // check '+' and '-'
+        if (*it == '+')
+            ++it;
+        else if (*it == '-') {
+            negative = true;
+            ++it;
+        }
+
+        // read digits
+        for (; it != s.end() && *it >= '0' && *it <= '9'; ++it) {
+           n = n * 10 + (*it - '0');
+
+            // verify if clamping is necessary
+            switch (negative) {
+                case true:
+                    if (n - 1 >= INT_MAX)
+                        return INT_MIN;
+                    
+                    break;
+
+                case false:
+                    if (n >= INT_MAX)
+                        return INT_MAX;
+            }
+        }
+
+        return negative ? -n : n;
+    }
+};

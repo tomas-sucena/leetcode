ea0434fb255d950c544989236be116bcbc0e1aae
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-07-13 01:36:51 +0100

    Time: 6 ms (75.63%), Space: 21.3 MB (15.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    void binarySearch(vector<int>& nums, int& index) {
+        int num = nums[index];
+        int lower = index, upper = nums.size() - 1;
+
+        while (lower <= upper) {
+            int middle = (lower + upper) / 2;
+
+            if (nums[middle] == num) {
+                lower = middle + 1;
+            }
+            else {
+                upper = middle - 1;
+            }
+        }
+
+        // update the index
+        index = upper;
+    }
+
+    int removeDuplicates(vector<int>& nums) {
+        int index = 0;
+        int unique = 0;
+
+        while (index < nums.size()) {
+            // search for the highest index where containing the current number
+            binarySearch(nums, index);
+            nums[unique++] = nums[index++];
+        }
+
+        return unique;
+    }
+
+    /**
+    // my original solution
     int removeDuplicates(vector<int>& nums) {
         int unique = 1;
         int prev = nums[0];
@@ -14,5 +48,5 @@ public:
         }
 
         return unique;
-    }
+    }**/
 };

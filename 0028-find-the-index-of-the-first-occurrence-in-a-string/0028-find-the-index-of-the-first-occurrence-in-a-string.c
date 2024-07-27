a07e56d00834b18b7b2f9c7aed8dae066d06cdbb
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-07-27 15:50:40 +0100

    Time: 3 ms (37.38%), Space: 7.7 MB (19.52%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -0,0 +1,19 @@
+int strStr(char* haystack, char* needle) {
+    int index = 0;
+
+    for (; *haystack; ++index) {
+        char *haystack_ = haystack++;
+        char *needle_ = needle;
+
+        while (*needle_ && *haystack_ && *needle_ == *haystack_) {
+            ++needle_; ++haystack_;
+        }
+
+        // verify if a match was found
+        if (*needle_ == '\0') {
+            return index;
+        }
+    }
+
+    return -1;
+}

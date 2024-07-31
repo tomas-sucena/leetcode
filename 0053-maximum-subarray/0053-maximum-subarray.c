62137599f23e7a965b7af196aa7339ce5a6f159b
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-07-31 11:07:51 +0100

    Time: 78 ms (65.22%), Space: 14.5 MB (19.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -4,32 +4,32 @@ int maxSubArray(int* nums, int numsSize) {
     int max = *nums, sum = max;
 
     for (int i = 1; i < numsSize; ++i) {
-        switch ((sum >= 0) << 1 | (nums[i] >= 0)) {
-            // negative sum, negative number
+        // determine if the current number and sum are negative
+        switch ((sum < 0) << 1 | (nums[i] < 0)) {
+            // positive sum, positive number
             case 0:
-                sum = MAX(nums[i], sum);
+                sum += nums[i];
                 max = MAX(sum, max);
 
                 break;
 
-            // negative sum, positive number
+            // positive sum, negative number
             case 1:
+                max = MAX(sum, max);
+                sum += nums[i];
+
+                break;
+
+            // negative sum, positive number
+            case 2:
                 sum = nums[i];
                 max = MAX(nums[i], max);
 
                 break;
 
-            // positive sum, negative number
-            case 2:
-                max = MAX(sum, max);
-                sum += nums[i];
-
-                break;
-            
-
-            // positive sum, positive number
+            // negative sum, negative number
             case 3:
-                sum += nums[i];
+                sum = MAX(nums[i], sum);
                 max = MAX(sum, max);
 
                 break;

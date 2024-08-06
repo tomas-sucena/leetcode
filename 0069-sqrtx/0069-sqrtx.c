3c21ad71e196312d50227606e86f5fc063db4903
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-08-06 23:34:31 +0100

    Time: 3 ms (59.25%), Space: 8.3 MB (37.4%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,20 @@
+int mySqrt(int x) {
+    int left = 0, right = 1 + x / 2;
+
+    while (left < right) {
+        int middle = 1 + (left + right) / 2;
+        int sqrt = x / middle;
+
+        if (sqrt == middle) {
+            return middle;
+        }
+        else if (sqrt < middle) {
+            right = middle - 1;
+        }
+        else {
+            left = middle;
+        }
+    }
+
+    return right;
+}

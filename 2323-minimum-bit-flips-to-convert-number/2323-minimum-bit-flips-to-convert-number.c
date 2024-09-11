ccf94baf1ac67cefd2c40cbcb3eb781debb4c609
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-09-11 11:21:14 +0100

    Time: 3 ms (44.58%), Space: 8 MB (12.05%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.c
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.c
@@ -1,13 +1,12 @@
 int minBitFlips(int start, int goal) {
     // XOR the numbers
     start ^= goal;
-    int mask = 1;
 
     // count the 1 bits
     int ans = 0;
 
-    for (int bit = 0; bit < 31; ++bit) {
-        ans += !!(start & 1 << bit);
+    for (unsigned mask = (unsigned) 1 << 31; mask; mask >>= 1) {
+        ans += !!(start & mask);
     }
 
     return ans;

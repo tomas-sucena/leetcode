96f49820a92e8f85085bbf3baf3974455f760c3d
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-09-18 00:00:29 +0100

    Time: 10 ms (64.71%), Space: 18.5 MB (8.82%) - LeetHub

--- 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.c
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.c
@@ -7,16 +7,17 @@
  * };
  */
 struct TreeNode* removeLeafNodes(struct TreeNode* root, int target) {
-    if (root == NULL) {
+    if (!root) {
         return NULL;
     }
 
-    root->left = removeLeafNodes(root->left, target);
-    root->right = removeLeafNodes(root->right, target);
+    bool toDelete = root->val == target;
+    toDelete &= !(root->left = removeLeafNodes(root->left, target));
+    toDelete &= !(root->right = removeLeafNodes(root->right, target));
 
     // determine if the current node is a leaf to be removed
     // if so, delete it
-    if (!root->left && !root->right && root->val == target) {
+    if (toDelete) {
         free(root);
         root = NULL;
     }

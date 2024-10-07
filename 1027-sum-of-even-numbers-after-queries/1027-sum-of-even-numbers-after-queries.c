0082b7da53c58b4671822c48aaac32ba3d046773
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-10-07 15:28:43 +0100

    Time: 158 ms (40%), Space: 27.4 MB (30%) - LeetHub

---
+++ 1027-sum-of-even-numbers-after-queries/1027-sum-of-even-numbers-after-queries.c
@@ -0,0 +1,37 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sumEvenAfterQueries(int* nums, int numsSize, int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
+    // compute the initial sum of even numbers
+    int sum = 0;
+
+    for (int i = 0; i < numsSize; ++i) {
+        sum += nums[i] * (nums[i] % 2 == 0);
+    }
+
+    // initialize the answer
+    int *ans = malloc(queriesSize * sizeof(int));
+    *returnSize = queriesSize;
+
+    // apply the queries and compute
+    for (int i = 0; i < queriesSize; ++i) {
+        int *query = queries[i];
+        int *num = nums + query[1]; // pointer to the affected number
+
+        // if the number is even, remove it from the sum
+        if (*num % 2 == 0) {
+            sum -= *num;
+        }
+
+        // update the number
+        *num += query[0];
+
+        // if the updated number is even, add it to the sum
+        sum += *num * (*num % 2 == 0);
+
+        // update the answer
+        ans[i] = sum;
+    }
+
+    return ans;
+}

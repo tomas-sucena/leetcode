aecf42e87a9b923068c624f5fa0ce5e3b683b045
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-10-17 14:29:19 +0100

    Time: 3 ms (63.16%), Space: 8.8 MB (50.88%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.c
+++ 3324-split-the-array/3324-split-the-array.c
@@ -1,31 +1,13 @@
-int lessThan(const void *a, const void *b) {
-    return *((int*) a) - *((int*) b);
-}
-
 bool isPossibleToSplit(int* nums, int numsSize) {
-    // sort the array
-    qsort(nums, numsSize, sizeof(int), lessThan);
+    unsigned char occurrences[101];
 
-    // ensure all numbers with even index are unique
-    int prev = -1;
+    // ensure all elements appear at most twice
+    for (int i = 0; i < numsSize; ++i) {
+        int num = nums[i];
 
-    for (int i = 0; i < numsSize; i += 2) {
-        if (nums[i] == prev) {
+        if (++occurrences[num] > 2) {
             return false;
         }
-
-        prev = nums[i];
-    }
-
-    // ensure all numbers with odd index are unique
-    prev = -1;
-
-    for (int i = 1; i < numsSize; i += 2) {
-        if (nums[i] == prev) {
-            return false;
-        }
-
-        prev = nums[i];
     }
 
     return true;

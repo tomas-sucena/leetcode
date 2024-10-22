e3bc90c1d4413c060abd7a1a45c2267f4d5c1703
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-10-22 16:26:37 +0100

    Time: 90 ms (48.72%), Space: 11.8 MB (58.46%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.c
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.c
@@ -20,14 +20,48 @@ bool rowBinarySearch(int *row, int rowSize, int target) {
     return false;
 }
 
-// O(m * log(n))
+// search a column for the target
+bool colBinarySearch(int **matrix, int col, int colSize, int target) {
+    int lower = 0, upper = colSize - 1;
+
+    while (lower <= upper) {
+        int mid = (lower + upper) / 2;
+        int num = matrix[mid][col];
+
+        if (num == target) {
+            return true;
+        }
+        else if (num < target) {
+            lower = mid + 1;
+        }
+        else {
+            upper = mid - 1;
+        }
+    }
+
+    return false;    
+}
+
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target){
     int n = *matrixColSize;
 
-    // search each row for the target
-    for (int i = 0; i < matrixSize && matrix[i][0] <= target; ++i) {
-        if (rowBinarySearch(matrix[i], n, target)) {
-            return true;
+    // determine which is faster - search the rows or the columns
+    if (matrixSize <= n) {
+        // search each row for the target
+        // O(m * log(n))
+        for (int i = 0; i < matrixSize && matrix[i][0] <= target; ++i) {
+            if (rowBinarySearch(matrix[i], n, target)) {
+                return true;
+            }
+        }
+    }
+    else {
+        // search each column for the target
+        // O(n * log(m))
+        for (int i = 0; i < n && matrix[0][i] <= target; ++i) {
+            if (colBinarySearch(matrix, i, matrixSize, target)) {
+                return true;
+            }
         }
     }
 

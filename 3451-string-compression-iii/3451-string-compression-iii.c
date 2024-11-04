dc492cd34aedaffa475bf17902f2a6173a98231c
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2024-11-04 13:20:40 +0000

    Time: 0 ms (100%), Space: 14.7 MB (87.5%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.c
+++ 3451-string-compression-iii/3451-string-compression-iii.c
@@ -1,19 +1,19 @@
 char* compressedString(char* word) {
     // initialize the compressed string
-    char *comp = malloc((2 * strlen(word) + 1) * sizeof(unsigned char)),
+    char *comp = malloc((2 * strlen(word) + 1) * sizeof(char)),
         *temp = comp;
 
     while (*word) {
         char c = *word++;
-        int count = 1;
+        char count = '1';
 
         // count the number of successive equal characters
-        while (*word == c && count < 9) {
+        while (*word == c && count < '9') {
             ++word;
             ++count;
         }
 
-        *temp++ = count + '0'; // append the number
+        *temp++ = count; // append the number
         *temp++ = c; // append the character
     }
 

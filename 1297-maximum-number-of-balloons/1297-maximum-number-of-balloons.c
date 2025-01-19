92e3e15e4291933a5e0a539d4b632dd87b287d25
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2025-01-19 13:36:53 -0300

    Time: 0 ms (100%), Space: 7.9 MB (44.68%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.c
@@ -0,0 +1,23 @@
+int min(int a, int b) {
+    return (a < b)
+        ? a
+        : b;
+}
+
+int maxNumberOfBalloons(char* text) {
+    // count the occurrences of each letter
+    int letters[26];
+
+    for (; *text; ++text) {
+        ++letters[*text % 'a'];
+    }
+
+    // determine how many times the word 'balloon'
+    // can be formed
+    return min(letters[1],          // 'b'
+        min(letters[0],             // 'a' 
+        min(letters['l' % 'a'] / 2, // 'l'
+        min(letters['o' % 'a'] / 2, // 'o'
+        letters['n' % 'a']          // 'n'
+    ))));
+}

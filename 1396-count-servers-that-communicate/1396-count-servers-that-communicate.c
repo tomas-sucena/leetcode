89020d534c9cb022e4c0d34e68a675718ec29063
Tomás Sucena Lopes <93825634+tomas-sucena@users.noreply.github.com>
2025-01-23 13:38:18 -0300

    Time: 0 ms (100%), Space: 13.4 MB (0%) - LeetHub

---
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.c
@@ -0,0 +1,38 @@
+int countServers(int** grid, int gridSize, int* gridColSize) {
+    int ans = 0, n = *gridColSize;
+    int *colServers = (int *) calloc(n, sizeof(int));
+
+    // visit the rows
+    // O(n * m)
+    for (int i = 0; i < gridSize; ++i) {
+        int servers = 0;
+
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j]) {
+                ++servers;
+                ++colServers[j];
+            }
+        }
+
+        // verify if the row has at least two servers
+        if (servers > 1) {
+            ans += servers; // update the answer
+            memset(grid[i], 0, n * sizeof(int));
+        }
+    }
+
+    // visit the columns
+    // O(n * m)
+    for (int j = 0; j < n; ++j) {
+        // skip unfeasible columns
+        if (colServers[j] <= 1) {
+            continue;
+        }
+
+        for (int i = 0; i < gridSize; ++i) {
+            ans += grid[i][j];
+        }
+    }
+
+    return ans;
+}
